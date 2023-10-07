2467e08397aaa30f4d959986e1bf2e1f65a5c95f
Kyle Corry <kylecorry31@gmail.com>
2023-10-07 13:05:42 -0400

    Use model with metadata

--- app/src/main/java/com/kylecorry/trail_sense/weather/domain/clouds/classification/TensorflowImageClassifier.kt
+++ app/src/main/java/com/kylecorry/trail_sense/weather/domain/clouds/classification/TensorflowImageClassifier.kt
@@ -2,18 +2,12 @@ package com.kylecorry.trail_sense.weather.domain.clouds.classification
 
 import android.content.Context
 import android.graphics.Bitmap
-import androidx.core.graphics.blue
-import androidx.core.graphics.green
-import androidx.core.graphics.red
 import com.kylecorry.andromeda.core.coroutines.onDefault
 import com.kylecorry.andromeda.core.coroutines.onIO
 import com.kylecorry.sol.math.statistics.Statistics
 import com.kylecorry.trail_sense.ml.CloudClassifierModel
-import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.model.Model
-import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
 
 class TensorflowImageClassifier(val context: Context) {
 
@@ -30,29 +24,9 @@ class TensorflowImageClassifier(val context: Context) {
         val model = loadModel()
 
         try {
-            val inputFeatures = TensorBuffer.createFixedSize(
-                intArrayOf(1, image.width, image.height, 3),
-                DataType.FLOAT32
-            )
-            val byteBuffer = ByteBuffer.allocateDirect(image.width * image.height * 3 * 4)
-            byteBuffer.order(ByteOrder.nativeOrder())
-
-            val pixels = IntArray(image.width * image.height)
-            image.getPixels(pixels, 0, image.width, 0, 0, image.width, image.height)
-
-            for (pixel in pixels) {
-                byteBuffer.putFloat(pixel.red.toFloat())
-                byteBuffer.putFloat(pixel.green.toFloat())
-                byteBuffer.putFloat(pixel.blue.toFloat())
-            }
-
-            inputFeatures.loadBuffer(byteBuffer)
-
-            val outputs = model.process(inputFeatures)
-
-            val values = outputs.outputFeature0AsTensorBuffer.floatArray.toList()
-
-            Statistics.softmax(values)
+            val outputs = model.process(TensorImage.fromBitmap(image))
+            val scores = outputs.probabilityAsCategoryList.sortedBy { it.index }.map { it.score }
+            Statistics.softmax(scores)
         } finally {
             model.close()
         }
