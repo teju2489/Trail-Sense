893dc7f8a8fae685d04ad9b266107dba0be48d11
Kyle Corry <kylecorry31@gmail.com>
2023-10-08 10:23:33 -0400

    Use generic TF classifier

--- app/src/main/java/com/kylecorry/trail_sense/weather/domain/clouds/classification/TensorflowCloudClassifier.kt
+++ app/src/main/java/com/kylecorry/trail_sense/weather/domain/clouds/classification/TensorflowCloudClassifier.kt
@@ -2,26 +2,27 @@ package com.kylecorry.trail_sense.weather.domain.clouds.classification
 
 import android.content.Context
 import android.graphics.Bitmap
-import com.kylecorry.andromeda.core.bitmap.BitmapUtils.resizeExact
 import com.kylecorry.sol.science.meteorology.clouds.CloudGenus
 import com.kylecorry.trail_sense.shared.ClassificationResult
 import com.kylecorry.trail_sense.shared.extensions.onDefault
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.ops.ResizeOp
 
 class TensorflowCloudClassifier(context: Context) : ICloudClassifier {
 
-    private val helper = TensorflowImageClassifier(context)
+    private val classifier = TensorflowImageClassifier(
+        context,
+        "cloud_classifier_model.tflite",
+        shouldApplySoftmax = true,
+        imageProcessor = ImageProcessor.Builder()
+            .add(ResizeOp(IMAGE_SIZE, IMAGE_SIZE, ResizeOp.ResizeMethod.BILINEAR))
+            .build()
+    )
 
 
     override suspend fun classify(bitmap: Bitmap): List<ClassificationResult<CloudGenus?>> =
         onDefault {
-            val resized = bitmap.resizeExact(IMAGE_SIZE, IMAGE_SIZE)
-            val results = try {
-                helper.classify(resized)
-            } finally {
-                if (resized != bitmap) {
-                    resized.recycle()
-                }
-            }
+            val results = classifier.classify(bitmap)
 
             val clouds = listOf(
                 CloudGenus.Altocumulus,
--- app/src/main/java/com/kylecorry/trail_sense/weather/domain/clouds/classification/TensorflowImageClassifier.kt
+++ app/src/main/java/com/kylecorry/trail_sense/weather/domain/clouds/classification/TensorflowImageClassifier.kt
@@ -5,30 +5,50 @@ import android.graphics.Bitmap
 import com.kylecorry.andromeda.core.coroutines.onDefault
 import com.kylecorry.andromeda.core.coroutines.onIO
 import com.kylecorry.sol.math.statistics.Statistics
-import com.kylecorry.trail_sense.ml.CloudClassifierModel
+import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
-import org.tensorflow.lite.support.model.Model
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
-class TensorflowImageClassifier(val context: Context) {
 
-    // To make this generic, consider using the Interpreter API instead: https://github.com/tensorflow/tflite-support/issues/534#issuecomment-865465219
-    private suspend fun loadModel(): CloudClassifierModel = onIO {
-        val options = Model.Options.Builder()
-            .setDevice(Model.Device.CPU)
-            .setNumThreads(4)
-            .build()
-        CloudClassifierModel.newInstance(context, options)
+class TensorflowImageClassifier(
+    private val context: Context,
+    private val modelFilePath: String,
+    private val imageProcessor: ImageProcessor = ImageProcessor.Builder().build(),
+    private val shouldApplySoftmax: Boolean = false,
+    private val numThreads: Int = 4,
+    private val useNNAPI: Boolean = false
+) {
+
+    private suspend fun loadModel(): Interpreter = onIO {
+        val options = Interpreter.Options()
+        options.numThreads = numThreads
+        options.useNNAPI = useNNAPI
+        val file = FileUtil.loadMappedFile(context, modelFilePath)
+        Interpreter(file, options)
     }
 
     suspend fun classify(image: Bitmap): List<Float> = onDefault {
-        val model = loadModel()
+        loadModel().use { model ->
+            val inputType = model.getInputTensor(0).dataType()
+            val input = TensorImage(inputType).also {
+                it.load(image)
+                imageProcessor.process(it)
+            }
 
-        try {
-            val outputs = model.process(TensorImage.fromBitmap(image))
-            val scores = outputs.probabilityAsCategoryList.sortedBy { it.index }.map { it.score }
-            Statistics.softmax(scores)
-        } finally {
-            model.close()
+            val outputType = model.getOutputTensor(0).dataType()
+            val outputShape = model.getOutputTensor(0).shape()
+            val output = TensorBuffer.createFixedSize(outputShape, outputType)
+            model.run(input.buffer, output.buffer)
+
+            val values = output.floatArray.toList()
+
+            if (shouldApplySoftmax) {
+                Statistics.softmax(values)
+            } else {
+                values
+            }
         }
     }
 }
